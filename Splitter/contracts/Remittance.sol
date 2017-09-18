061d7b9fb0d363740172848987e57d78590b4dca
sathish <njsathya@hotmail.com>
2017-09-18 21:33:47 +0400

    newer version of remittance

--- Splitter/contracts/Remittance.sol
+++ Splitter/contracts/Remittance.sol
@@ -5,6 +5,7 @@ contract Remittance {
    
     address public escrowOwner;
     uint public escrowFee;
+    address firstPartyAccount;
    
     struct FirstParty{
         uint amount;
@@ -12,13 +13,21 @@ contract Remittance {
         uint deadline;
     }
 
-    function Remittance(){
+    function Remittance(address firstParty){
         escrowOwner = msg.sender;
-        escrowFee = 10000 ;
+        escrowFee = tx.gasprice -1;
+        firstPartyAccount = firstParty;
+        
     }
     
     mapping(address=>uint) public balances;
     mapping(address=>FirstParty) public firstParty;
+    
+    modifier validateOwner() {
+      require(escrowOwner == msg.sender);
+    _;
+   }
+  
 
     function startTransaction(bytes32 password1, bytes32 password2, uint duration)
     public
@@ -34,14 +43,17 @@ contract Remittance {
     }
     
 
-    function requestFundsfromParty1(bytes32 password1, bytes32 password2, address firstPartyAddress)
+    function requestFundsfromParty1(bytes32 password1, bytes32 password2, address firstPartyAddress) 
     public 
     returns(bool success){
         require(firstParty[firstPartyAddress].deadline>block.number);
         require(firstParty[firstPartyAddress].passwordHash == keccak256(password1,password2)); 
         firstParty[firstPartyAddress].deadline = 0; 
-        firstParty[firstPartyAddress].amount = 0; 
-        msg.sender.transfer(firstParty[firstPartyAddress].amount);
+        
+        uint amount =firstParty[firstPartyAddress].amount;
+        firstParty[firstPartyAddress].amount = 0;
+        
+        msg.sender.transfer(amount);
         return true;
     }
 
@@ -50,12 +62,22 @@ contract Remittance {
       returns(bool success){
         require(firstParty[msg.sender].deadline<=block.number);
         require(firstParty[msg.sender].amount>0);
+        
+        uint amount = firstParty[msg.sender].amount;
         firstParty[msg.sender].amount = 0;
         firstParty[msg.sender].deadline = 0;
-        msg.sender.transfer(firstParty[msg.sender].amount);
+        msg.sender.transfer(amount);
         return true;
     }
-}
+     
+   function kill() 
+    validateOwner()
+    public 
+  {
+    selfdestruct(escrowOwner);
+   }
+
+  }
 
 
 
